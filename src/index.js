f329b61ba263d86b9408282ffdc748afc5ebeb92
Phyllis Wong <wong.phyllism@gmail.com>
2022-10-11 16:05:55 -0600

    feat: update signin to signout when a user has signed in

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+
+import { UserProvider } from './components/contexts/user.context';
 
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
