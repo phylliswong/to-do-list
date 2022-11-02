2bfa2ff12dbda9cfc1201e162d169c1208a2c24a
Phyllis Wong <wong.phyllism@gmail.com>
2022-11-01 18:08:24 -0600

    fix: auth page styles

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -7,7 +7,6 @@ import {
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
-import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
