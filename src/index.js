8b49e0e4fea7e46f46b6b255d1e84fcb9c11d217
Phyllis Wong <wong.phyllism@gmail.com>
2022-11-11 14:35:14 -0700

    feat: add datadog for real user monitoring (#13)
    * add datadog to packages
    * initialize datadog

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { datadogRum } from '@datadog/browser-rum';
 
 import App from './App';
 
@@ -9,6 +10,23 @@ import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 
 import './index.css';
+
+datadogRum.init({
+  applicationId: 'b55167dc-2c1e-44f0-89ff-c25e26057881',
+  clientToken: 'pubabfc8ac11d57f5eb5c5d87ff280bcdfc',
+  site: 'datadoghq.com',
+  service:'todo-list',
+  
+  version: '1.0.0',
+  sampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel:'mask-user-input'
+});
+  
+datadogRum.startSessionReplayRecording();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
